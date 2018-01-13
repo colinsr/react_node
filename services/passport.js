afd66f89fc6ce6403854f03cfce2916209f52fe5
Colin Detwiller <colinsr@me.com>
2018-01-13 11:28:47 -0500

    fix order of require statements

--- services/passport.js
+++ services/passport.js
@@ -1,6 +1,9 @@
 const passport = require('passport');
 const GoogleStrategy = require('passport-google-oauth20').Strategy;
 const keys = require('../config/keys');
+const mongoose = require('mongoose');
+
+const User = mongoose.model('users');
 
 passport.use(
   new GoogleStrategy({
@@ -8,8 +11,6 @@ passport.use(
     clientSecret: keys.googleClientSecret,
     callbackURL: '/auth/google/callback'
   }, (accessToken, refreshToken, profile, done) => {
-    console.log(accessToken);
-    console.log(refreshToken);
-    console.log(profile);
+    new User({ googleId: profile.id }).save();
   })
 );
