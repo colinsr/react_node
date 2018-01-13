2a1f4849103ba9266bfba6f10951b6e226387852
Colin Detwiller <colinsr@me.com>
2018-01-13 16:19:24 -0500

    rename

--- models/user.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require('mongoose');
-const { Schema } = mongoose;
-//const Schema = mongoose.Schema; these are the same thing
-
-const userSchema = new Schema({
-  googleId: String
-});
-
-mongoose.model('users', userSchema);
