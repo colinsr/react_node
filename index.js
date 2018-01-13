8b38953bb5a2aec931f6ff39d7493cd085d71cd4
Colin Detwiller <colinsr@me.com>
2018-01-13 13:26:51 -0500

    wire up express/passport storing encyrpted token in cookie

--- index.js
+++ index.js
@@ -1,12 +1,24 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const keys = require('./config/keys');
+const cookieSession = require('cookie-session');
+const passport = require('passport');
 require('./models/User');
 require('./services/passport');
 
 mongoose.connect(keys.mongoUri);
 
 const app = express();
+
+app.use(
+  cookieSession({
+    maxAge: 30 * 24 * 60 * 60 * 1000, //30 days worth of milliseconds
+    keys: [ keys.cookieKey ]
+  })
+);
+app.use(passport.initialize());
+app.use(passport.session());
+
 require('./routes/authRoutes')(app);
 
 const PORT = process.env.PORT || 5000;
